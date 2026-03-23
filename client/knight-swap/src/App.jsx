b76773a1fe843749a6434f8fd9875f899d75ebae
Koicob <jacobw318@live.com>
2026-03-22 23:17:13 -0400

    fixx3

--- client/knight-swap/src/App.jsx
+++ client/knight-swap/src/App.jsx
@@ -4,8 +4,7 @@ import Login from './pages/login';
 import Register from './pages/register';
 import Seller from './pages/seller';
 import Admin from './pages/admin';
-import Buyer from './pages/buyer'; 
-import Homepage from './pages/homepage'; 
+
 
 
 export default function App() {
@@ -14,17 +13,15 @@ export default function App() {
             <div>
                 <h1>Knight Swap Marketplace</h1>
                 <hr />
-                
-                {/* The Routes control which component shows up based on the URL */}
+             
                 <Routes>
-                    {/* The project requires the first screen to be the login page */}
+  
                     <Route path="/" element={<Login />} />
                     
                     <Route path="/register" element={<Register />} />
                     <Route path="/seller" element={<Seller />} />
                     <Route path="/admin" element={<Admin />} />
-                    
-                    {/* <Route path="/buyer" element={<Buyer />} /> */}
+              
                 </Routes>
             </div>
         </BrowserRouter>
